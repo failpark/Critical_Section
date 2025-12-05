1d36c12d5e415d4ebc205d6aee13954ed9b3c24c
Hefner, Rene <rene.hefner@study.thws.de>
2025-12-05 19:19:43 +0100

    Initial commit

---
+++ critical/nodes/node4.py
@@ -0,0 +1,22 @@
+import socket
+import time
+import random
+
+COORD_IP = '192.168.1.101'
+PORT = 5000
+
+def run_node():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_id = f"Node_4"
+
+    while True:
+        time.sleep(random.uniform(2, 5))
+        sock.sendto(f"REQ {my_id}".encode(), (COORD_IP, PORT))
+        sock.recv(1024)
+
+        time.sleep(random.uniform(2, 4))
+
+        sock.sendto(f"REL {my_id}".encode(), (COORD_IP, PORT))
+
+if __name__ == "__main__":
+    run_node()
